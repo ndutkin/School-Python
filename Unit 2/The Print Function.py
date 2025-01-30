2dc75988e038e522ebab3db07df3600ee92dc2ba
Nathaniel Dutkin <ndutkin@students.mchenry.edu>
2025-01-29 20:51:50 -0600

    Delete Unit 2 directory

--- Unit 2/The Print Function.py
+++
@@ -1,8 +0,0 @@
-# Name: Nathaniel Dutkin
-# Github: https://github.com/ndutkin/School-Python
-
-#Knock-Knock Joke Program, one line of code.
-print("Knock, knock.\nWho's there?\nGorilla.\nGorilla who?\nGorilla me a hamburger, I'm hungry!")
-
-#Phrase Program, one line of code. Uses the sep argument to separate the words with special characters.
-print("I","Hope","The","Bills","Win",sep="!*~ ")
