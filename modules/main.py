ae2cce56c4ce22f211aa73da434e2ce2dd750fcc
Nathaniel Dutkin <ndutkin@students.mchenry.edu>
2025-03-10 15:52:30 -0500

    Add files via upload

---
+++ modules/main.py
@@ -0,0 +1,8 @@
+from math_operations import calculator
+
+result = calculator.add(5, 3)
+print("Addition result:", result)
+
+result = calculator.subtract(10, 4)
+print("Subtraction result:", result)
+
---
+++ modules/math_operations/geometry.py
@@ -0,0 +1,6 @@
+def recarea(a, b):
+    return a * b
+def triarea(a, b):
+    return 0.5 * a * b
+def circlearea(r):
+    return 3.14 * r * r 
